7bcccda341f2aa347e95419dc96ec52465b4f46d
Galina-Novikova <74134068+Galina-Novikova@users.noreply.github.com>
2022-11-17 17:55:57 +0300

    Someone add few lines

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('....')
+    print('Awesome!!!')
+    print('UFO came and add this line')
 
 
 if __name__ == '__main__':
